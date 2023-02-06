79a5cece37d5f3c1833b62415234d1d4de37cd59
kassiopereirabm <kassio.lima.p@gmail.com>
2023-02-06 16:25:19 -0300

    time out na resposta do chatgpt

--- services/open_ai_service.py
+++ services/open_ai_service.py
@@ -4,14 +4,16 @@ from os import getenv
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 _LIMIT_TOKENS = 4096
-_LIMIT_TOKENS_PROMPT = _LIMIT_TOKENS / 2
+_LIMIT_TOKENS_PROMPT = int(_LIMIT_TOKENS / 2)
+
 
 def _contarTokens(text: str) -> int:
-    tokens = tokenizer(text)["input_ids"]
+    tokens = tokenizer(text).data["input_ids"]
     return len(tokens)
 
+
 def cortarMensagemParaCaberNosTokens(mensagem) -> str:
-    tokens_ids = tokenizer(mensagem)["input_ids"]
+    tokens_ids = tokenizer(mensagem).data['input_ids']
 
     if len(tokens_ids) <= _LIMIT_TOKENS_PROMPT:
         return mensagem
@@ -19,6 +21,7 @@ def cortarMensagemParaCaberNosTokens(mensagem) -> str:
         tokens_ids = tokens_ids[-_LIMIT_TOKENS_PROMPT:]
         mensagemCortada = tokenizer.decode(tokens_ids)
         return mensagemCortada
+
 
 async def EnviarMensagem(mensagemRecebida: str) -> str:
     n_tokens = _contarTokens(mensagemRecebida)
@@ -36,7 +39,12 @@ async def EnviarMensagem(mensagemRecebida: str) -> str:
         "Content-Type": "application/json"
     }
 
-    response = requests.post("https://api.openai.com/v1/completions", json=params, headers=headers)
-    resposta = response.json()["choices"][0]["text"]
+    resposta = ""
+
+    try:
+        response = requests.post("https://api.openai.com/v1/completions", json=params, headers=headers, timeout=(60 * 5))
+        resposta = response.json()["choices"][0]["text"]
+    except:
+        resposta = "Desculpe, não consegui pensar em uma resposta 😕"
 
     return resposta
