892c87eadf5f043064547f0478fc5bbd9c14d787
kassiopereirabm <kassio.lima.p@gmail.com>
2023-02-06 19:26:58 -0300

    renomeando comandos

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ application = Application.builder().token(getenv("TELEGRAM_BOT_KEY")).build()
 
 application.add_handler(CommandHandler("start", telegramService.start))
 application.add_handler(CommandHandler("novaconversa", telegramService.novaConversa))
-application.add_handler(CommandHandler("listarconversas", telegramService.listarConversas))
+application.add_handler(CommandHandler("minhasconversas", telegramService.listarConversas))
 application.add_handler(CommandHandler("apagarconversa", telegramService.apagarConversa))
-application.add_handler(CommandHandler("continuarconversa", telegramService.continuarConversa))
+application.add_handler(CommandHandler("mudarconversa", telegramService.continuarConversa))
 application.add_handler(MessageHandler(filters.TEXT, telegramService.mensagemRecebida))
 application.add_handler(CallbackQueryHandler(telegramService.callback_handler))
 
