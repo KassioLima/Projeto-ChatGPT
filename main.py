f668bdee72b74ca7676ca9a687d76c2caa864770
kassiopereirabm <kassio.lima.p@gmail.com>
2023-03-08 23:30:25 -0300

    dando print para nao parar o workflow

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from telegram.ext import CommandHandler, MessageHandler, filters, Application, C
 from os import getenv
 from dotenv import load_dotenv
 from services import telegram_service as telegramService
+from threading import Thread
+import time
 
 load_dotenv()
 application = Application.builder().token(getenv("TELEGRAM_BOT_KEY")).build()
@@ -14,5 +16,19 @@ application.add_handler(CommandHandler("mudarconversa", telegramService.continua
 application.add_handler(CommandHandler("gerarimagem", telegramService.gerarimagem))
 application.add_handler(MessageHandler(filters.TEXT, telegramService.mensagemRecebida))
 application.add_handler(CallbackQueryHandler(telegramService.callback_handler))
+
+
+class Th(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        while True:
+            print("Rodando " + str(time.time()))
+            time.sleep(5)
+
+
+Th().start()
+
 
 application.run_polling()
