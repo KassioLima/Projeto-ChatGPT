7eb248f5e0581faae5c697e8abd708e2c01be2fd
kassiopereirabm <kassio.lima.p@gmail.com>
2023-03-10 16:51:57 -0300

    usando flask para escutar na porta 8081

--- main.py
+++ main.py
@@ -23,6 +23,18 @@ class Th(Thread):
         Thread.__init__(self)
 
     def run(self):
+
+        from flask import Flask
+
+        app = Flask(__name__)
+
+        @app.route('/')
+        def hello_world():  # put application's code here
+            return 'Hello World!'
+
+        if __name__ == '__main__':
+            app.run(host='0.0.0.0', port=8081)
+
         while True:
             print("Rodando " + str(time.time()))
             time.sleep(5)
